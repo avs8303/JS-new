0750b76e3fe03d288cef804ede50470bde2a8491
avs8303 <134585613+avs8303@users.noreply.github.com>
2026-03-06 21:27:49 +0000

    ..

---
+++ dataTypes.js
@@ -0,0 +1,20 @@
+// Primitive data types
+
+let age = 23    // number 
+let name = 'Anshu' // string 
+let loggedIn = true;  // boolean
+let preference = null // null
+let sum ;  // undefined 
+let id = Symbol("id") // symbol
+let num2 = 2314545935348254357909457893425092345704587n; // bigint
+
+// non-primitive
+
+// object 
+
+const details ={
+    name:'Anshu',
+    age: 25,
+    city: 'Lucknow'
+}
+console.log(details)
---
+++ typecoercion.js
@@ -0,0 +1,6 @@
+// Implicit coercion
+
+console.log("5" -1)
+console.log(2-"6")
+console.log("2"-"6")
+console.log(true+3)
